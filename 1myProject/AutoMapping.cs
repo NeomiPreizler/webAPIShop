e399c3f6f79479abf4f52c6b0954a43a98a9710c
fraidyEfrat <36214264632@mby.co.il>
2023-05-14 18:42:45 +0300

    cart display-correct cart delete one item left doesnt work

--- 1myProject/AutoMapping.cs
+++ 1myProject/AutoMapping.cs
@@ -16,5 +16,5 @@ namespace _1myProject
             CreateMap<UserLoginDTO, User>();
         }
     }
-}
+
 }
--- DL/ProductDL.cs
+++ DL/ProductDL.cs
@@ -34,10 +34,10 @@ namespace DL
             //        (maxPrice == null || p.Price <= maxPrice))
             //        .OrderBy(p => p.Price).ToListAsync();
             //return products;
-            public async Task<IEnumerable<Product>> Get(IEnumerable<string>? categories, string? nameProduct, int? minPrice, int? maxPrice, string? orderBy = "name", string? direction = "desc")
+            public async Task<IEnumerable<Product>> Get(IEnumerable<int>? categories, string? nameProduct, int? minPrice, int? maxPrice, string? orderBy = "name", string? direction = "desc")
             {
                 return await _myShopDbContext.Products.Include(p => p.Category).Where(p =>
-                    (categories.Count() == 0 ? true : !categories.Contains(p.Category.CategoryName)) &&
+                    (categories.Count() == 0 ? true : categories.Contains(p.Category.CategoryId)) &&
                     (nameProduct == null || p.ProductName.Contains(nameProduct)) &&
                     (minPrice == null || p.Price >= minPrice) &&
                     (maxPrice == null || p.Price <= maxPrice))
