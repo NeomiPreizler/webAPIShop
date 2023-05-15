f41196cc5e44094d10db773b2c07f3a431c12b89
fraidyEfrat <36214264632@mby.co.il>
2023-05-15 15:59:56 +0300

    middleware

---
+++ 1myProject/ErrorHandlingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace _1myProject
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        ILogger<ErrorHandlingMiddleware> _logger;
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation("error caught in middlware", e.Message," ",e.StackTrace);
+
+                httpContext.Response.StatusCode = 500;
+
+                await httpContext.Response.WriteAsync("internal error in server");
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline...
+    public static class ErrorHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseErrorHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorHandlingMiddleware>();
+        }
+    }
+}
